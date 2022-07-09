729330d831db08b2840029e5e6119a21e1a0b0ed
Josh DeCristi <jdecristi777a@gmail.com>
2022-07-09 00:45:25 -0400

    Fixed typos and UI changes in about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,9 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import MainLayout from '../src/layouts/MainLayout';
-import { Container, Typography, Grid, Card } from '@mui/material';
+import { Box, Container, Typography, Grid, Card } from '@mui/material';
+import Architecture from '@mui/icons-material/Architecture';
+import MonitorIcon from '@mui/icons-material/Monitor';
 
 const About: NextPage = () => {
    //State and Hooks
@@ -11,20 +13,23 @@ const About: NextPage = () => {
    return (
       <>
          <MainLayout link={{ name: 'products', href: '/' }}>
-            <Container sx={{ mt: '10rem', mb: '5rem' }}>
+            <Container sx={{ mt: '10rem', mb: 0 }}>
                <Typography variant="h2" color="primary">
                   Who I am
                </Typography>
-               {/* <Image src="" /> */}
                <Card sx={{ my: '2rem', p: '2.5rem' }}>
-                  <Typography sx={{ my: '1rem' }}>
-                     I started with a youtube video on web development using basic HTML and CSS. In that first hour, I realised just how amzing code is.
+                  <Typography sx={{ mb: '1rem' }}>
+                     My journey into web development started with a simple YouTube video that showed me how to use basic HTML and CSS. In that first hour, I was exposed to just how
+                     amazing coding is and my curiosity was sparked to learn more.
                   </Typography>
                   <Typography sx={{ my: '1rem' }}>
-                     After a few months of coding, I found a job at That’s Us Technologies. There I was surrounded by developers who taught me way more than I could have ever
-                     learned in such a short time.
+                     After a few months of coding on my own time, I landed a job at That’s Us Technologies. There, I was surrounded by developers that were able to teach me far
+                     more than I could have learned on my own. This opportunity allowed me to expand my knowledge of the field and opened my eyes to the variety within it.
                   </Typography>
-                  <Typography>Now I find myself interested in building beautiful, interactive, user interfaces, front end architecture.</Typography>
+                  <Typography sx={{ mt: '1rem' }}>
+                     Currently, I am interested in building out clean and appealing interactive user interfaces that utilize effective front end architecture. My goal is to one day
+                     be able to move into blockchain development and deal with crypto currencies.
+                  </Typography>
                </Card>
             </Container>
             <Container sx={{ my: '5rem' }}>
@@ -32,14 +37,29 @@ const About: NextPage = () => {
                   What I love
                </Typography>
                <Card sx={{ my: '2rem', p: '2.5rem', display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, alignItems: 'center', gap: '3rem' }}>
-                  <Image src="/images/svg/code.svg" width="75" height="75" alt="</>" />
-                  <Typography>I love designing and building beautiful, and interactive user interfaces. I believe that a users expierence is a</Typography>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem' }}>
+                     <MonitorIcon color="primary" sx={{ width: '3rem', height: '3rem' }} />
+                     <Typography color="primary" align="center">
+                        <b>User Experience</b>
+                     </Typography>
+                  </Box>
+                  <Typography>
+                     One of the most important parts of any website or application is the user interface, as people’s experience with it is what will keep them on the app or site.
+                     This is why I love to put extra time into designing them to be not only appealing, but also interactive. A good user experience leads to more time on the app
+                     or site and a happier client.
+                  </Typography>
                </Card>
                <Card sx={{ my: '2rem', p: '2.5rem', display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, alignItems: 'center', gap: '2rem' }}>
-                  <Image src="/images/svg/crypto.svg" width="150" height="150" alt="Bitcoin & Ethereum" />
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem' }}>
+                     <Architecture color="primary" sx={{ width: '3rem', height: '3rem' }} />
+                     <Typography color="primary" align="center">
+                        <b>Architecture</b>
+                     </Typography>
+                  </Box>
                   <Typography>
-                     I have a fascination with crypto currencies, and the blockchains that run them. The idea of descentralized systems are interesting. While I am not a blockchain
-                     developer, I do love to learn about the code that runs these systems.
+                     Architecture can be the make or break of any fast scaling app which is why it is so important. The speed in which a project can scale relies on how the
+                     architecture behind the code affects the features within it. Implementing good architecture makes the code cleaner, which in turn makes the developers’ lives
+                     easier and happier.
                   </Typography>
                </Card>
             </Container>
@@ -47,15 +67,22 @@ const About: NextPage = () => {
                <Typography variant="h2" color="primary">
                   Skills I have
                </Typography>
-               <Grid container columns={4} spacing="2rem" sx={{ m: 'auto' }}>
+               <Box
+                  sx={{
+                     mt: { xs: '2.5rem', sm: '5rem', md: '5rem' },
+                     display: 'grid',
+                     gridTemplateColumns: { xs: '1fr 1fr', sm: '1fr 1fr 1fr', md: '1fr 1fr 1fr 1fr' },
+                     rowGap: { xs: '2rem', sm: '5rem', md: '5rem' },
+                  }}
+               >
                   {skills.map((skill) => (
-                     <Grid key={skills.indexOf(skill)} item sm={1}>
-                        <Card sx={{ maxWidth: '100px', p: '1.5rem' }}>
+                     <Box>
+                        <Card sx={{ mx: 'auto', maxWidth: '100px', p: '1.5rem' }}>
                            <Image src={`/images/svg/${skill}.svg`} width="100" height="100" alt={`${skill} logo`} />
                         </Card>
-                     </Grid>
+                     </Box>
                   ))}
-               </Grid>
+               </Box>
             </Container>
          </MainLayout>
       </>

01b4712d239ecb52d683711e5bae2f3efce21e9f
Josh DeCristi <jdecristi777a@gmail.com>
2023-02-16 23:47:09 -0400

    Added pulse animation for image loading

--- src/components/main/FlipCard/ImageCard.tsx
+++ src/components/main/FlipCard/ImageCard.tsx
@@ -9,12 +9,34 @@ interface Props {
 
 export default ({ height, width, url, alt }: Props) => (
   <ImageCard sx={{ height, width, boxShadow: '5px 5px #00000095' }}>
+    <Loading>Loading...</Loading>
     <Image sx={url ? { backgroundImage: `url("${url}")` } : { backgroundColor: '#555555', '::Before': { content: `"${alt}"` } }} />
   </ImageCard>
 );
 
 const ImageCard = styled(Card)({
   padding: '10px',
+  position: 'relative',
+});
+
+const Loading = styled(Box)({
+  width: 'calc(100% - 20px)',
+  height: 'calc(100% - 20px)',
+  color: '#000000',
+  backgroundColor: '#999999',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'absolute',
+  top: '10px',
+  left: '10px',
+  animation: 'pulse 2500ms linear infinite',
+
+  '@keyframes pulse': {
+    '  0%': { backgroundColor: '#999999' },
+    ' 50%': { backgroundColor: '#99999975' },
+    '100%': { backgroundColor: '#999999' },
+  },
 });
 
 const Image = styled(Box)({

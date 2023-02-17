1c54e325b72837c3124b51df67d64f1cfa2a6bd3
Josh DeCristi <jdecristi777a@gmail.com>
2023-02-16 21:55:25 -0400

    Fixed About card spacing issue on mobile

--- src/components/main/layouts/sections/About.tsx
+++ src/components/main/layouts/sections/About.tsx
@@ -8,10 +8,10 @@ export default () => {
 
   return (
     <Section id={0}>
-      <Stack mt={{ xs: '25vh', md: '30vh', lg: '40vh' }} direction="row" justifyContent="center" alignItems="center" gap={{ md: '10%', lg: '5%' }}>
+      <Stack mt={{ xs: '30vh', lg: '40vh' }} direction="row" justifyContent="center" alignItems="center" gap={{ md: '10%', lg: '5%' }}>
         {!isSmall && <ImageCard width={{ md: '30vw', lg: '360px' }} height={{ md: '35vw', lg: '360px' }} url="/images/main/profile.jpeg" alt="Profile Image" />}
-        <TextCard width={{ xs: '100%', sm: '450px', md: '60vw', lg: '600px' }}>
-          {isSmall && <Image />}
+        <TextCard width={{ xs: '90%', sm: '450px', md: '60vw', lg: '600px' }}>
+          {isSmall && <Image sx={{ my: { xs: '5%', md: 0 }, width: { xs: '70%', md: '100%' } }} />}
           Utilizing problem-solving skills to tackle complex real-world issues and create simple yet robust solutions using my extensive knowledge of React.js, Vue.js,
           and Typescript. Committed to staying up-to-date with the latest industry trends and continuously improving my skills while effectively communicating complex
           topics clearly and simply.
@@ -22,8 +22,7 @@ export default () => {
 };
 
 const Image = styled(Box)({
-  mb: '5%',
-  width: '100%',
+  margin: 'auto',
   aspectRatio: '11 / 10',
   background: 'url("/images/main/profile.jpeg")',
   backgroundRepeat: 'no-repeat',
